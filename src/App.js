803bd91420b2c748891bb0bb906df229b3931be5
Sarah Cheatham <sarahcheatham@Sarahs-iMac.attlocal.net>
2019-06-08 14:40:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+   constructor(){
+     super();
+    this.state = {
+      users: []
+    }
+    fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(users => this.setState({users: users.users}))
+    .catch(error => console.log(error));
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+{/* <li> {users.map(user => {
+                  return user.username
+                })}
+          </li> */}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header>Calendar</header>
+    )
+}
+
+export default Header;
