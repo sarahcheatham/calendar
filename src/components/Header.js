72d67f482e5cce67d2fb5d455139ef9b0350d95b
Sarah Cheatham <sarah.cheatham14@gmail.com>
2019-06-09 20:29:35 -0500

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Header = (props) => {
+    console.log("PROPS:", props)
    const userName = props.users.map(user=> user.username);
     return (
         <header id="calhead">
